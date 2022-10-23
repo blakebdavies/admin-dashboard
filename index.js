5c49d64773b5c2966a78f215a0770476f4986411
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-23 09:54:28 +0100

    created order data and updated table data

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ themeToggle.addEventListener('click', () => {
     themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+//fill orders in table
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.shipping === 'Declined' ? 'danger' 
+        : order.shipping === 'Pending' ? 'warning' : 'success'}">${order.shipping}</td>
+        <td class="primary">Details</td>
+`;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Canon EOS R6',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Nikon D780',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Sony α7 III',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Fujifilm X-T4',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Panasonic Lumix DC-S5',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Olympus OM-D E-M5 Mark III',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
